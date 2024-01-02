7261a3ebfa4f17715f71031e260e43a4378b1470
Johannes Krabbe <Johannes@Krabbe.net>
2024-01-02 14:34:48 +0100

    api: test fix

--- packages/api/tests/indexController.test.ts
+++ packages/api/tests/indexController.test.ts
@@ -1,7 +1,7 @@
 import app from "../src/app";
 import { test, expect } from "bun:test";
 
-test("GET /hello is ok", async () => {
-  const res = await app.request("/hello");
+test("GET / => healthcheck", async () => {
+  const res = await app.request("/");
   expect(res.status).toBe(200);
 });
