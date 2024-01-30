93ced391b839abed4833bef503651eb3d8eb0a20
Johannes Krabbe <Johannes@Krabbe.net>
2024-01-30 23:56:57 +0100

    start working on zod auth middleware

--- packages/api/src/controllers/auth/auth.controller.ts
+++ packages/api/src/controllers/auth/auth.controller.ts
@@ -1,8 +1,26 @@
 import { Hono } from 'hono'
-import { ENV } from '../../env'
+import { zValidator } from '@hono/zod-validator'
+import { z } from 'zod'
 
 export const authController = new Hono()
 
 authController.get('/verify-email', (c) => {
     return c.text('This is the Entas API', 200)
 })
+
+const schema = z.object({
+    name: z.string(),
+    age: z.string(),
+})
+
+authController.post(
+    '/post',
+    zValidator('json', schema, (result, c) => {
+        if (!result.success) {
+            return c.text('Invalid!', 400)
+        }
+    }),
+    (c) => {
+        return c.json({ success: true }, 200)
+    }
+)
